dcf1f1cbd1ee5a21069cecd5ad3ace09e3889988
YU-Cheng Yang <142778228+Yycheng092@users.noreply.github.com>
2026-03-02 14:46:16 +0800

    Update What_is_Module.py

--- What_is_Module.py
+++ What_is_Module.py
@@ -1,1 +1,29 @@
+"""
+在 Python 中 module 就是一個 .py 檔案
+只要一個 Python 檔案，可以被其他程式 import，那它就是一個 module
 
+在模組中裡面通常可以放  1.variable 2.function 3.class(物件) 
+
+file : math_tools.py    # math_tools.py 即為一個模組
+PI = 3.14159    # 1.variable
+
+def add(a,b):    # 2.function
+  return a + b 
+
+class Calculator :    #3. class 
+  def multiply(self , a , b):
+    return a * b 
+
+所以在 math_tools.py 的結構中是這樣
+math_tools (module)
+ ├─ PI (variable)
+ ├─ add (function)
+ └─ Calculator (class)
+
+那如何使用呢?
+如果有另一個程式 import math_tools
+那麼他就可以使用裡面的內容，像是
+math_tools.PI
+math_tools.add(2,3)
+math_tools.Calculator()
+"""
